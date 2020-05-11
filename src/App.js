033b437d8ae8331f8d0672c23b5298992109111c
Ignacio <54366484+auyeros@users.noreply.github.com>
2020-05-11 16:46:16 -0300

    Create react context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeProvider } from "./context/ThemeContext";
 
 class App extends Component {
     render() {
         return (
-            <>
-                <NavBar />
-                <Form />
-            </>
+            <ThemeProvider>
+                <PageContent>
+                    <NavBar />
+                    <Form />
+                </PageContent>
+            </ThemeProvider>
         );
     }
 }
