6898826e60cfeb1e4233c3978c0750939e6adc82
Ignacio <54366484+auyeros@users.noreply.github.com>
2020-05-11 15:56:43 -0300

    Added Responsive Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import Form from "./Form";
 
 class App extends Component {
     render() {
-        return <NavBar />;
+        return (
+            <>
+                <NavBar />
+                <Form />
+            </>
+        );
     }
 }
 export default App;
