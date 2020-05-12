35c7e318e065d5015754b50d055ff47244ccbc9d
Ignacio <54366484+auyeros@users.noreply.github.com>
2020-05-12 17:13:41 -0300

    Rewrite ThemeContext and Added custom Hooks

---
+++ src/hooks/useToggleState.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function useToggle(initialVal = false) {
+    // call useState, "reserve pieee of state"
+    const [state, setState] = useState(initialVal);
+    const toggle = () => {
+        setState(!state);
+    };
+
+    // return piece of sate AND a function to toggle it
+    return [state, toggle];
+}
+export default useToggle;
